f704a8153dfd699428e6080bf96339c674df2b5a
himanshu goyal <himanshugoyal1011@gmail.com>
2020-11-18 10:28:21 +0530

    Deleted some unneccessory file

--- application/helpers/pdf_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-function tcpdf()
-{
-    require_once('tcpdf/config/lang/eng.php');
-    require_once('tcpdf/tcpdf.php');
-}
